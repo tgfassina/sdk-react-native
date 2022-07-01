5ff49b8743e380d534de52a573c3b5cfcb548c66
Thiago Fassina <thiagoaaa@gmail.com>
2022-07-01 15:14:34 -0300

    Add prettier

--- storybook/index.js
+++ storybook/index.js
@@ -1,7 +1,7 @@
 import {
-  getStorybookUI,
-  configure,
-  addDecorator,
+	getStorybookUI,
+	configure,
+	addDecorator,
 } from "@storybook/react-native";
 import { withKnobs } from "@storybook/addon-knobs";
 
@@ -13,14 +13,14 @@ import "./rn-addons";
 addDecorator(withKnobs);
 
 Font.loadAsync({
-  THICCCBOI_ExtraBold: require("../assets/fonts/THICCCBOI-ExtraBold.ttf"),
-  THICCCBOI_Bold: require("../assets/fonts/THICCCBOI-Bold.ttf"),
-  THICCCBOI_Medium: require("../assets/fonts/THICCCBOI-Medium.ttf"),
-  THICCCBOI_Regular: require("../assets/fonts/THICCCBOI-Regular.ttf"),
 }),
   configure(async () => {
     require("./stories");
   }, module);
+	THICCCBOI_ExtraBold: require("../assets/fonts/THICCCBOI-ExtraBold.ttf"),
+	THICCCBOI_Bold: require("../assets/fonts/THICCCBOI-Bold.ttf"),
+	THICCCBOI_Medium: require("../assets/fonts/THICCCBOI-Medium.ttf"),
+	THICCCBOI_Regular: require("../assets/fonts/THICCCBOI-Regular.ttf"),
 
 const StorybookUIRoot = getStorybookUI({});
 
