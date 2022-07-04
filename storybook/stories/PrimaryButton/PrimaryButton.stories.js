90b7ab6297f57e986d99e3d7a6ba37873ddd16b7
Thiago Fassina <thiagoaaa@gmail.com>
2022-07-04 18:16:14 -0300

    Buttons can be clicked

--- storybook/stories/PrimaryButton/PrimaryButton.stories.js
+++ storybook/stories/PrimaryButton/PrimaryButton.stories.js
@@ -1,14 +1,8 @@
-import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react-native";
 import React from "react";
 import PrimaryButton from ".";
 import CenterViewDark from "../CenterViewDark";
 
 storiesOf("PrimaryButton", module)
-  .addDecorator((getStory) => <CenterViewDark>{getStory()}</CenterViewDark>)
-  .add("with text", () => (
-    <PrimaryButton
-      onPress={action("clicked-text")}
-      text="Hello"
-    ></PrimaryButton>
-  ));
+	.addDecorator((getStory) => <CenterViewDark>{getStory()}</CenterViewDark>)
+	.add("with text", () => <PrimaryButton onPress={() => {}} text="Hello" />);
--- storybook/stories/SecondaryButton/SecondaryButton.stories.js
+++ storybook/stories/SecondaryButton/SecondaryButton.stories.js
@@ -1,12 +1,8 @@
-import { action } from "@storybook/addon-actions";
 import { storiesOf } from "@storybook/react-native";
 import React from "react";
-import { View } from "react-native";
 import SecondaryButton from ".";
 import CenterViewDark from "../CenterViewDark";
 
 storiesOf("SecondaryButton", module)
 	.addDecorator((getStory) => <CenterViewDark>{getStory()}</CenterViewDark>)
-	.add("with text", () => (
-		<SecondaryButton onPress={action("clicked-text")} text="Hello2" />
-	));
+	.add("with text", () => <SecondaryButton onPress={() => {}} text="Hello2" />);
