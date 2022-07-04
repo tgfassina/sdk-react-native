0a56d7cfff83c360faf0bd3b8b3822da2c7a275f
henrique <hvhallmann@gmail.com>
2022-07-04 13:32:14 -0300

    Remove logo with text and use one with svg text and png icon

--- storybook/stories/SlidingUpPanel/index.tsx
+++ storybook/stories/SlidingUpPanel/index.tsx
@@ -1,24 +1,35 @@
 import React, { useEffect, useRef } from "react";
+import { StyleSheet } from "react-native";
 import SlidingUpPanel from "rn-sliding-up-panel";
 
 type Props = {
-  show: number;
-  children: any;
-  createButtonHandler?: any;
-  secureButtonHandler?: any;
-  skipButtonHandler?: any;
+	show: number;
+	children: any;
+	createButtonHandler?: any;
+	secureButtonHandler?: any;
+	skipButtonHandler?: any;
 };
 
 const SlidingUpPanelComponent = (props: Props) => {
-  const slideRef = useRef(null);
-  useEffect(() => {
-    if (props.show > 0) {
-      slideRef.current.show(props.show);
-    } else {
-      slideRef.current.hide();
-    }
-  }, [props.show]);
-  return <SlidingUpPanel ref={slideRef}>{props.children}</SlidingUpPanel>;
+	const slideRef = useRef(null);
+	useEffect(() => {
+		if (props.show > 0) {
+			slideRef.current.show(props.show);
+		} else {
+			slideRef.current.hide();
+		}
+	}, [props.show]);
+	return (
+		<SlidingUpPanel containerStyle={styles.container} ref={slideRef}>
+			{props.children}
+		</SlidingUpPanel>
+	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		borderRadius: 26,
+	},
+});
 
 export default SlidingUpPanelComponent;
