d60ccb39032fbf0f4f70012576aa624590a66623
Henrique Hallmann <hvhallmann@gmail.com>
2022-07-06 18:30:24 -0300

    Improve icon text style using props

--- storybook/stories/Input/IconText.tsx
+++ storybook/stories/Input/IconText.tsx
@@ -30,83 +30,73 @@ type Props = {
 const IconText: React.FC<Props> = (props: Props) => {
   const Icon = iconSvg[props.icon];
 
-  let fillColor = undefined;
   let borderStyle = undefined;
   if (props.warning) {
-    fillColor = "#FF2866";
     borderStyle = { borderColor: "#FF2866" };
   }
 
-  let labelStyle = undefined;
-  let inputStyle = undefined;
-  let textInputStyle = undefined;
-  if (props.lightTheme) {
-    labelStyle = { color: "#4F4A60" };
-    inputStyle = { borderColor: "#9893A2" };
-    textInputStyle = { color: "#37324A" };
-  }
-
   return (
-    <View style={styles.containerInput}>
-      <Text style={[styles.labelText, labelStyle]}>{props.label}</Text>
-      <View style={[styles.input, inputStyle, borderStyle]}>
-        {Icon({ fillColor })}
+    <View style={styles(props).containerInput}>
+      <Text style={[styles(props).labelText]}>{props.label}</Text>
+      <View style={[styles(props).input, borderStyle]}>
+        {Icon({ fillColor: props.warning && "#FF2866" })}
         <TextInput
-          style={[styles.textInput, textInputStyle]}
+          style={[styles(props).textInput]}
           value={props.text}
           onChangeText={props.onChangeText}
           secureTextEntry={props.secureTextEntry}
         />
         {props.warning && <WarningOutline />}
       </View>
-      <Text style={styles.warningText}>{props.warning}</Text>
+      <Text style={styles(props).warningText}>{props.warning}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  containerInput: {
-    flexDirection: "column",
-    justifyContent: "center",
-    width: "100%",
-    paddingHorizontal: 40,
-  },
-  input: {
-    flexDirection: "row",
-    borderColor: "#F6F5FA",
-    borderStyle: "solid",
-    borderWidth: 1,
-    borderRadius: 6,
-    alignItems: "center",
-    paddingHorizontal: 12,
-  },
-  labelText: {
-    fontFamily: "THICCCBOI_Regular",
-    fontStyle: "normal",
-    textAlign: "left",
-    fontSize: 14,
-    lineHeight: 16,
-    color: "#F6F5FA",
-    marginBottom: 8,
-  },
-  textInput: {
-    flex: 1,
-    paddingVertical: 12,
-    marginLeft: 12,
-    fontFamily: "THICCCBOI_Regular",
-    fontStyle: "normal",
-    fontSize: 16,
-    lineHeight: 20,
-    color: "#F6F5FA",
-  },
-  warningText: {
-    fontFamily: "THICCCBOI_Medium",
-    fontStyle: "normal",
-    textAlign: "left",
-    fontSize: 12,
-    lineHeight: 16,
-    color: "#FF2866",
-  },
-});
+const styles = (props) =>
+  StyleSheet.create({
+    containerInput: {
+      flexDirection: "column",
+      justifyContent: "center",
+      width: "100%",
+      paddingHorizontal: 40,
+    },
+    input: {
+      flexDirection: "row",
+      borderColor: props.lightTheme ? "#F6F5FA" : "#9893A2",
+      borderStyle: "solid",
+      borderWidth: 1,
+      borderRadius: 6,
+      alignItems: "center",
+      paddingHorizontal: 12,
+    },
+    labelText: {
+      fontFamily: "THICCCBOI_Regular",
+      fontStyle: "normal",
+      textAlign: "left",
+      fontSize: 14,
+      lineHeight: 16,
+      color: props.lightTheme ? "#4F4A60" : "#F6F5FA",
+      marginBottom: 8,
+    },
+    textInput: {
+      flex: 1,
+      paddingVertical: 12,
+      marginLeft: 12,
+      fontFamily: "THICCCBOI_Regular",
+      fontStyle: "normal",
+      fontSize: 16,
+      lineHeight: 20,
+      color: props.lightTheme ? "#37324A" : "#F6F5FA",
+    },
+    warningText: {
+      fontFamily: "THICCCBOI_Medium",
+      fontStyle: "normal",
+      textAlign: "left",
+      fontSize: 12,
+      lineHeight: 16,
+      color: "#FF2866",
+    },
+  });
 
 export default IconText;
--- storybook/stories/SecondaryButton/index.tsx
+++ storybook/stories/SecondaryButton/index.tsx
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontFamily: "THICCCBOI_ExtraBold",
     color: "#F6F5FA",
+    letterSpacing: 1,
   },
 });
 
