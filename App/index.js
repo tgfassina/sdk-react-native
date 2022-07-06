dbeb98c9af426e0947688e0a514fe6d08b05c63e
Thiago Fassina <thiagoaaa@gmail.com>
2022-07-06 18:27:11 -0300

    Change expo entry point

--- App/index.js
+++ App/index.js
@@ -1,1 +1,14 @@
-export { default } from "../storybook";
+import { LogBox } from "react-native";
+LogBox.ignoreLogs([
+  "ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from 'deprecated-react-native-prop-types'.",
+  "EventEmitter.removeListener('change', ...): Method has been deprecated. Please instead use `remove()` on the subscription returned by `EventEmitter.addListener`.",
+]);
+
+import { registerRootComponent } from "expo";
+
+import App from "../storybook";
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
