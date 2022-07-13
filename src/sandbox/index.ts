e803d8f7a4b192c6dee7aff09353286e8e546761
Thiago Fassina <thiagoaaa@gmail.com>
2022-07-13 17:52:24 -0300

    Delete stactic database

--- src/sandbox/index.ts
+++ src/sandbox/index.ts
@@ -1,27 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const database = [
-  {
-    username: "bob",
-    password: "doug",
-    matrixUsername: "usera",
-    matrixPassword: "eBY89ZF8JeXqadJNihhwx3cZH2TK7K",
-  },
-  {
-    username: "doug",
-    password: "bob",
-    matrixUsername: "userb",
-    matrixPassword: "9S9Wihktc49YYCAvg3h3Fv3KKyNbDF",
-  },
-];
-
 const login = async (username: string, password: string) => {
   const sessionDB = await AsyncStorage.getItem("database");
-  const database = sessionDB != null ? JSON.parse(sessionDB) : null;
-
-  if (!database) {
-    return null;
-  }
+  const database = JSON.parse(sessionDB || "[]");
 
   const user = database.find((user) => user.username === username);
   if (!user || user.password !== password) {
@@ -47,16 +28,10 @@ const createAccount = async (username: string, password: string) => {
   };
 
   const sessionDB = await AsyncStorage.getItem("database");
-  if (!sessionDB) {
-    database.push(newRecord);
-    const jsonValue = JSON.stringify(database);
-    AsyncStorage.setItem("database", jsonValue);
-  } else {
-    const jsonValue: any[] = JSON.parse(sessionDB);
-    jsonValue.push(newRecord);
-    const strJsonValue = JSON.stringify(jsonValue);
-    AsyncStorage.setItem("database", strJsonValue);
-  }
+  const database = JSON.parse(sessionDB || "[]");
+  database.push(newRecord);
+  AsyncStorage.setItem("database", JSON.stringify(database));
+
   return true;
 };
 
